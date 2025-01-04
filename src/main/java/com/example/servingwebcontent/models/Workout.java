4994fc444f81629188e7e71b138306bdabf49d35
va0rlov <orlov.topsbi@gmail.com>
2025-01-04 17:55:33 +0300

    'id' added in model

--- src/main/java/com/example/servingwebcontent/models/Workout.java
+++ src/main/java/com/example/servingwebcontent/models/Workout.java
@@ -1,12 +1,18 @@
 package com.example.servingwebcontent.models;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record Workout(
-        String workoutName, // Название тренировки
-        String workoutType, // Тип тренировки
-        int durationInMinutes, // Продолжительность тренировки в минутах
-        int caloriesBurned, // Количество сожженных калорий
-        LocalDateTime workoutDate, // Дата и время тренировки
-        String notes // Дополнительные заметки
-) {}
+public record Workout(UUID id, // Уникальный идентификатор тренировки
+                      String workoutName, // Название тренировки
+                      String workoutType, // Тип тренировки
+                      int durationInMinutes, // Продолжительность тренировки в минутах
+                      int caloriesBurned, // Количество сожженных калорий
+                      LocalDateTime workoutDate, // Дата и время тренировки
+                      String notes // Дополнительные заметки
+) {
+    // Конструктор для создания новой тренировки с автоматической генерацией UUID
+    public Workout(String workoutName, String workoutType, int durationInMinutes, int caloriesBurned, LocalDateTime workoutDate, String notes) {
+        this(UUID.randomUUID(), workoutName, workoutType, durationInMinutes, caloriesBurned, workoutDate, notes);
+    }
+}
