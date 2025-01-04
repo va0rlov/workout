37825f66b654e02aa5d7c5054c9590520d0a0ebc
va0rlov <orlov.topsbi@gmail.com>
2025-01-04 21:55:41 +0300

    'update' form && functionality added

--- src/main/java/com/example/servingwebcontent/controllers/WorkoutWebController.java
+++ src/main/java/com/example/servingwebcontent/controllers/WorkoutWebController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller // Используем @Controller для возврата HTML
 @RequestMapping("/") // Базовый путь для веб-интерфейса
@@ -42,6 +44,24 @@ public class WorkoutWebController {
     @GetMapping("/new")
     public String showCreateForm() {
         return "create-workout"; // Возвращаем шаблон формы создания тренировки
+    }
+
+    /**
+     * Отображает форму для редактирования существующей тренировки.
+     *
+     * @param id    ID тренировки для редактирования
+     * @param model модель для передачи данных в шаблон
+     * @return имя шаблона (edit-workout.html)
+     */
+    @GetMapping("/edit/{id}")
+    public String showEditForm(@PathVariable UUID id, Model model) {
+        Workout workout = workoutService.getWorkoutById(id);
+        if (workout != null) {
+            model.addAttribute("workout", workout); // Передаем тренировку в форму
+            return "edit-workout"; // Возвращаем шаблон формы редактирования
+        } else {
+            return "redirect:/"; // Если тренировка не найдена, перенаправляем на корневой путь
+        }
     }
 
 }
